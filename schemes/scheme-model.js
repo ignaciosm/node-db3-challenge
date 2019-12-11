5704786acc5fd34b75654221ed8dd17030924e46
ignaciosm <ignaciosm@gmail.com>
2019-12-11 17:34:14 -0500

    mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,57 @@
 const db = require("../data/db-config.js");
 
 module.exports = {
-  find
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
 }
 
 function find() {
   return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({id}).first();
+}
+
+// select * from steps
+// join schemes
+// on steps.scheme_id = schemes.id
+// where schemes.id = 1
+
+function findSteps(schemeId) {
+  return db("steps")
+    .select('steps.id', 'schemes.scheme_name', 'steps.instructions')
+    .join('schemes', 'steps.scheme_id', 'schemes.id')
+    .where('scheme_id', schemeId)
+}
+
+function add(scheme) {
+  return db("schemes")
+  .insert(scheme, 'id')
+  .then(ids => {
+    const [id] = ids;
+    return findById(id);
+  })
+}
+
+function update(changes, id) {
+  return db("schemes")
+  .where({id})
+  .update(changes)
+  .then(scheme => {
+    return scheme
+  })
+}
+
+function remove(id) {
+  return db("schemes")
+  .where({id})
+  .delete()
+  .then(scheme => {
+    return null
+  })
 }
